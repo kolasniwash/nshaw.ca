6fec93f4b57f29b2720ee7d9d0a3aeff629a8565
kolasniwash <nickashaw3@gmail.com>
2024-02-22 17:36:10 +0100

    automate cloud run iac deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,12 +9,7 @@ terraform {
 
 provider "google" {
   project     = "nshawca"
-  region      = "us-west1"
-}
-
-
-resource "random_id" "bucket_prefix" {
-  byte_length = 8
+  region      = "europe-west1"
 }
 
 resource "google_storage_bucket" "default" {
@@ -27,69 +22,24 @@ resource "google_storage_bucket" "default" {
   }
 }
 
-resource "google_compute_network" "vpc_network" {
-  name                    = "my-custom-mode-network"
-  auto_create_subnetworks = false
-  mtu                     = 1460
-}
+resource "google_cloud_run_v2_service" "default" {
+  name     = "blog"
+  location = "europe-west1"
+  client   = "terraform"
 
-resource "google_compute_subnetwork" "default" {
-  name          = "my-custom-subnet"
-  ip_cidr_range = "10.0.1.0/24"
-  region        = "us-west1"
-  network       = google_compute_network.vpc_network.id
-}
-
-# Create a single Compute Engine instance
-resource "google_compute_instance" "default" {
-  name         = "flask-vm"
-  machine_type = "f1-micro"
-  zone         = "us-west1-a"
-  tags         = ["ssh"]
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
-  }
-
-  # Install Flask
-  metadata_startup_script = "sudo apt-get update; sudo apt-get install -yq build-essential python3-pip rsync; pip install flask"
-
-  network_interface {
-    subnetwork = google_compute_subnetwork.default.id
-
-    access_config {
-      # Include this section to give the VM an external IP address
+  template {
+    containers {
+      image = "gcr.io/nshawca/nshawca-webserver:latest"
+      ports {
+        container_port = 80
+      }
     }
   }
 }
 
-resource "google_compute_firewall" "ssh" {
-  name = "allow-ssh"
-  allow {
-    ports    = ["22"]
-    protocol = "tcp"
-  }
-  direction     = "INGRESS"
-  network       = google_compute_network.vpc_network.id
-  priority      = 1000
-  source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["ssh"]
-}
-
-resource "google_compute_firewall" "flask" {
-  name    = "flask-app-firewall"
-  network = google_compute_network.vpc_network.id
-
-  allow {
-    protocol = "tcp"
-    ports    = ["5000"]
-  }
-  source_ranges = ["0.0.0.0/0"]
-}
-
-// A variable for extracting the external IP address of the VM
-output "Web-server-URL" {
- value = join("",["http://",google_compute_instance.default.network_interface.0.access_config.0.nat_ip,":5000"])
+resource "google_cloud_run_v2_service_iam_member" "noauth" {
+  location = google_cloud_run_v2_service.default.location
+  name     = google_cloud_run_v2_service.default.name
+  role     = "roles/run.invoker"
+  member   = "allUsers"
 }
