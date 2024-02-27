8a241e011443ed94fa8f304b44c928b77bae3c2d
kolasniwash <nickashaw3@gmail.com>
2024-02-27 11:28:28 +0100

    build cloud run based on image sha from artefact registry

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,4 +3,14 @@ terraform {
     bucket  = "nshawca-tfstate"
     prefix  = "terraform/state"
   }
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "2.13.0"
+    }
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.17.0"
+    }
+  }
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 5.17.0"
-    }
-  }
-}
 
 provider "google" {
   project     = "nshawca"
@@ -29,7 +21,7 @@ resource "google_cloud_run_v2_service" "default" {
 
   template {
     containers {
-      image = "gcr.io/nshawca/nshawca-webserver:latest"
+      image = data.google_container_registry_image.nshawca_latest.image_url
       ports {
         container_port = 80
       }
@@ -42,4 +34,25 @@ resource "google_cloud_run_v2_service_iam_member" "noauth" {
   name     = google_cloud_run_v2_service.default.name
   role     = "roles/run.invoker"
   member   = "allUsers"
+}
+
+
+data "google_client_config" "default" {}
+
+provider "docker" {
+  registry_auth {
+    address = "gcr.io"
+    username = "oauth2accesstoken"
+    password = data.google_client_config.default.access_token
+  }
+}
+
+data "docker_registry_image" "nshawca_image" {
+  name = "gcr.io/nshawca/nshawca-webserver:latest"
+}
+
+data "google_container_registry_image" "nshawca_latest" {
+  name = "nshawca-webserver"
+  project = "nshawca"
+  digest = data.docker_registry_image.nshawca_image.sha256_digest
 }
